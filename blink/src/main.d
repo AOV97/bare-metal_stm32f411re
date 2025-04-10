1baed85e54caef8f8422f99a0d736c5c56d8155c
aov <ovillamor21@gmail.com>
2025-04-10 14:47:56 -0700

    Adding systick to get more accurate delay for the blinking LED

--- blink/src/main.d
+++ blink/src/main.d
@@ -1,18 +1,22 @@
-src/main.o: src/main.c \
- ../../bare-metal-series/libopencm3/include/libopencm3/stm32/rcc.h \
- ../../bare-metal-series/libopencm3/include/libopencm3/cm3/common.h \
+src/main.o: src/main.c ../libopencm3/include/libopencm3/stm32/rcc.h \
+ ../libopencm3/include/libopencm3/cm3/common.h \
  /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
  /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
- ../../bare-metal-series/libopencm3/include/libopencm3/stm32/memorymap.h \
- ../../bare-metal-series/libopencm3/include/libopencm3/stm32/f4/memorymap.h \
- ../../bare-metal-series/libopencm3/include/libopencm3/cm3/memorymap.h \
- ../../bare-metal-series/libopencm3/include/libopencm3/stm32/f4/rcc.h \
- ../../bare-metal-series/libopencm3/include/libopencm3/stm32/pwr.h \
- ../../bare-metal-series/libopencm3/include/libopencm3/stm32/f4/pwr.h \
- ../../bare-metal-series/libopencm3/include/libopencm3/stm32/common/pwr_common_v1.h \
- ../../bare-metal-series/libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
- ../../bare-metal-series/libopencm3/include/libopencm3/stm32/gpio.h \
- ../../bare-metal-series/libopencm3/include/libopencm3/stm32/f4/gpio.h \
- ../../bare-metal-series/libopencm3/include/libopencm3/stm32/common/gpio_common_f24.h \
- ../../bare-metal-series/libopencm3/include/libopencm3/stm32/common/gpio_common_f234.h \
- ../../bare-metal-series/libopencm3/include/libopencm3/stm32/common/gpio_common_all.h
+ ../libopencm3/include/libopencm3/stm32/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/f4/memorymap.h \
+ ../libopencm3/include/libopencm3/cm3/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/f4/rcc.h \
+ ../libopencm3/include/libopencm3/stm32/pwr.h \
+ ../libopencm3/include/libopencm3/stm32/f4/pwr.h \
+ ../libopencm3/include/libopencm3/stm32/common/pwr_common_v1.h \
+ ../libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ ../libopencm3/include/libopencm3/stm32/gpio.h \
+ ../libopencm3/include/libopencm3/stm32/f4/gpio.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_f24.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_f234.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_all.h \
+ ../libopencm3/include/libopencm3/cm3/systick.h \
+ ../libopencm3/include/libopencm3/cm3/vector.h \
+ ../libopencm3/include/libopencm3/cm3/nvic.h \
+ ../libopencm3/include/libopencm3/dispatch/nvic.h \
+ ../libopencm3/include/libopencm3/stm32/f4/nvic.h
