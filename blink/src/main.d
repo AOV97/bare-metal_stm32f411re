53da7d751b639b0762a4d689a3f77e021e98b0a8
aov <ovillamor21@gmail.com>
2025-04-10 13:25:44 -0700

    first commit blinking LED

---
+++ blink/src/main.d
@@ -0,0 +1,18 @@
+src/main.o: src/main.c \
+ ../../bare-metal-series/libopencm3/include/libopencm3/stm32/rcc.h \
+ ../../bare-metal-series/libopencm3/include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
+ ../../bare-metal-series/libopencm3/include/libopencm3/stm32/memorymap.h \
+ ../../bare-metal-series/libopencm3/include/libopencm3/stm32/f4/memorymap.h \
+ ../../bare-metal-series/libopencm3/include/libopencm3/cm3/memorymap.h \
+ ../../bare-metal-series/libopencm3/include/libopencm3/stm32/f4/rcc.h \
+ ../../bare-metal-series/libopencm3/include/libopencm3/stm32/pwr.h \
+ ../../bare-metal-series/libopencm3/include/libopencm3/stm32/f4/pwr.h \
+ ../../bare-metal-series/libopencm3/include/libopencm3/stm32/common/pwr_common_v1.h \
+ ../../bare-metal-series/libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ ../../bare-metal-series/libopencm3/include/libopencm3/stm32/gpio.h \
+ ../../bare-metal-series/libopencm3/include/libopencm3/stm32/f4/gpio.h \
+ ../../bare-metal-series/libopencm3/include/libopencm3/stm32/common/gpio_common_f24.h \
+ ../../bare-metal-series/libopencm3/include/libopencm3/stm32/common/gpio_common_f234.h \
+ ../../bare-metal-series/libopencm3/include/libopencm3/stm32/common/gpio_common_all.h
